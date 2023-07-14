0f9ddcee6b2d592b33581ff7641feba28c49f7a2
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-07-14 08:20:53 -0500

    Create Units

--- src/app/crud/modules/units/pages/units-form-page/units-form-page.component.ts
+++ src/app/crud/modules/units/pages/units-form-page/units-form-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { UnitsService } from '../../units.service';
+import { UnitReqBody } from '../../unit.interface';
 
 @Component({
   selector: 'units-form-page',
@@ -9,14 +11,26 @@ import { Router } from '@angular/router';
 export class UnitsFormPageComponent {
   private fb = inject(FormBuilder);
   private router = inject(Router);
+  private unitsService = inject(UnitsService);
 
   public newUnitForm: FormGroup = this.fb.group({
     unit: ['', Validators.required],
   });
 
+  get unit(): UnitReqBody {
+    return this.newUnitForm.value as UnitReqBody;
+  }
+
   public onSubmit(): void {
     if (this.newUnitForm.invalid) return this.newUnitForm.markAllAsTouched();
 
-    this.router.navigateByUrl('/crud/units/list');
+    console.log(this.unit);
+    this.unitsService.create(this.unit).subscribe({
+      next: () => this.router.navigateByUrl('/crud/units/list'),
+      error: (e) => {
+        this.newUnitForm.reset();
+        console.error(e);
+      },
+    });
   }
 }
